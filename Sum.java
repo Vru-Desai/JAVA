ac92e490122e0f9eb82fba7755ab745b968fcfe1
Vru-Desai <vrunda22desai@gmail.com>
2020-02-05 10:46:49 +0530

    CollectionDemo file added

---
+++ Sum.java
@@ -0,0 +1,20 @@
+
+public class Sum {
+
+	static int a=10;
+	static int b=20;
+	
+	public static void main(String[] args) {
+		Sum s = new Sum();
+		
+		System.out.println(s.sum("vrunda",10));
+	
+	}
+    
+	<A>A sum(A a,A b)
+	{
+		System.out.println(a);
+		System.out.println(b);
+		return a;
+	}
+}
